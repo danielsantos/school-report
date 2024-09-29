a1322c1164b6b226fe6eebac8d055a05eb06b11b
Daniel dos Santos Rocha <daniel.arocha@mercadolivre.com>
2024-09-29 18:14:37 -0300

    wip

---
+++ src/test/kotlin/com/teoali/school_report/SchoolReportApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.teoali.school_report
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SchoolReportApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
