323ebfe2bc3a5c9c827b09a1b846a20abc378547
Daniel dos Santos Rocha <daniel.arocha@mercadolivre.com>
2024-09-29 17:42:12 -0300

    scaffolding

---
+++ src/main/kotlin/com/teoali/school_report/SchoolReportApplication.kt
@@ -0,0 +1,11 @@
+package com.teoali.school_report
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SchoolReportApplication
+
+fun main(args: Array<String>) {
+	runApplication<SchoolReportApplication>(*args)
+}
