97bd6187883205951f55cd4e11b415450185a011
Daniel dos Santos Rocha <daniel.arocha@mercadolivre.com>
2024-09-29 17:52:47 -0300

    wip

---
+++ src/main/kotlin/com/teoali/school_report/HomeController.kt
@@ -0,0 +1,14 @@
+package com.teoali.school_report
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HomeController {
+
+    @GetMapping
+    fun home(): String {
+        return "Meu Boletim Escolar >> Em construção..."
+    }
+
+}
