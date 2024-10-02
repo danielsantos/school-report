684f33806813e209cbca800ca2cbfe51a0962301
Daniel dos Santos Rocha <daniel.arocha@mercadolivre.com>
2024-10-01 23:03:30 -0300

    wip

--- src/main/kotlin/com/teoali/school_report/HomeController.kt
+++ src/main/kotlin/com/teoali/school_report/HomeController.kt
@@ -1,13 +1,13 @@
 package com.teoali.school_report
 
+import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
 
-@RestController
+@Controller
 class HomeController {
 
-    @GetMapping
+    @GetMapping("/")
     fun home(): String {
-        return "home/index"
+        return "index"
     }
 }
