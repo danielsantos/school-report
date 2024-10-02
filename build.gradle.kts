cf6075ee5a1e249db775006e985cf7048e5096c6
Daniel dos Santos Rocha <daniel.arocha@mercadolivre.com>
2024-10-01 22:42:32 -0300

    wip

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,8 +21,8 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	providedRuntime("org.springframework.boot:spring-boot-starter-tomcat")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
